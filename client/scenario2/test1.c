741a7d6e693a70b77cc4f217680b9471a19470cf
sam33 <sup3355@gmail.com>
2016-12-08 09:26:44 +0800

    add scenario

---
+++ client/scenario2/test1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "address_space_survey.h"
+#define SIZE 1000000
+void main()
+{ 
+    int result[1024];
+    memset(result,0x0,sizeof(int)*1024);
+    int a[SIZE]; 
+    memset(a,0x0,sizeof(int)*SIZE);
+    printf("Hello World!\n");
+    address_space_survey(768,1023, result);
+    int i;
+    for(i=0 ; i<1024 ; i++) {
+        if(result[i]!=0)
+            printf("index[%d]=%d\n",i,result[i]);
+    } 
+}   
---
+++ client/scenario2/test2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "address_space_survey.h"
+#include <string.h>
+#define SIZE 1000000
+int a[SIZE]; 
+void main()
+{ 
+    int result[1024] = {1};
+    memset(result,0x0,sizeof(int)*1024);
+    memset(a,0x0,sizeof(int)*SIZE);
+    printf("Hello World!\n");
+    address_space_survey(768,1023, result);
+    int i;
+    for(i=0 ; i<1024 ; i++) {
+        if(result[i]!=0)
+            printf("index[%d]=%d\n",i,result[i]);
+    } 
+}   
